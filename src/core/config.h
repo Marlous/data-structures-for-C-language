eae40811c9a6bd6955731994d2cca284226eebe2
猫小毛 <moskylit@gmail.com>
2019-02-22 23:28:00 +0800

    change structure of project
    completed !

---
+++ src/core/config.h
@@ -0,0 +1,13 @@
+/*
+ * config.h
+ *
+ *  Created on: 2019年2月20日
+ *      Author: Mo
+ */
+
+#ifndef SRC_CORE_CONFIG_H_
+#define SRC_CORE_CONFIG_H_
+
+#include "linear_list.h"
+
+#endif /* SRC_CORE_CONFIG_H_ */
